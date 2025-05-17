1e9716fe2c26783f55d027272928bdb1df3dff05
Wh1tEW0lf13 <miloszmis13@gmail.com>
2025-05-17 16:28:23 +0200

    Joł joł

---
+++ GraphsAlgorythms/MST.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Wh1tEW0lf13 on 17.05.2025.
+//
+
+#include "MST.h"
---
+++ GraphsAlgorythms/MST.h
@@ -0,0 +1,16 @@
+//
+// Created by Wh1tEW0lf13 on 17.05.2025.
+//
+
+#ifndef MST_H
+#define MST_H
+
+
+
+class MST {
+
+};
+
+
+
+#endif //MST_H
